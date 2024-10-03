38ac685dc95e3bd4743c617f5de3ab2d85bc3bf5
Djuniorr <danieljuniorpf9@gmail.com>
2024-10-03 19:54:39 -0300

    Feat: Create image docker

--- api/db.js
+++ api/db.js
@@ -1,15 +1,15 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "login"
+    host: process.env.DB_HOST || "localhost",  // O host pode ser definido pela variável de ambiente
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "123456",
+    database: process.env.DB_NAME_LOGIN || "login"
 })
 
 export const ubs_db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "ubs"
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "123456",
+    database: process.env.DB_NAME_UBS || "ubs"
 })
