6b8bcaffa2fa0fd98942c6e380d9af1a0be8358f
Juliano Corso <122948556+JuliCorso@users.noreply.github.com>
2024-11-10 15:33:47 -0300

    Update global.js

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -2,17 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  body {
+    width: 100%;
+    height: 100%;
+    background-color: #657997;
+    font-family: Calibri, Arial;
+    margin: 0px;
   }
 
-  body {
-    width: 100vw;
-    height: 100vh;
-    background-color: #657997;
-    font-family: Arial, Helvetica, sans-serif
+  &#root{
+    width: 100%;
+    height: 100%;
   }
 `;
 
