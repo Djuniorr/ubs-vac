dd93d6ae3390f74a3690b6f23ea22257c648e73b
Juliano Corso <122948556+JuliCorso@users.noreply.github.com>
2024-11-10 15:32:01 -0300

    Estilização

--- frontend/src/pages/Ubs/index.js
+++ frontend/src/pages/Ubs/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }  from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import useAuth from "../../hooks/useAuth";
@@ -14,7 +14,7 @@ const Ubs = () => {
     setNome('');
   }
   console.log(nome);
-  
+
   useEffect(() => {
     getUbs();
   }, [getUbs]);
@@ -24,37 +24,53 @@ const Ubs = () => {
   return (
     <C.Container>
       <C.Menu>
-        <C.Title>UBS VAC</C.Title>
-        <Button Text="Home" onClick={() => [getUbs(), navigate("/home")]} />
-        <Button Text="Encontrar UBS" onClick={() => [getUbs(), navigate("/home")]} />
-        <Button Text="Vacinas" onClick={() => [getUbsWithVacinas(), navigate("/vacinas")]} />
-        <Button Text="Feedback" onClick={() => [signout(), navigate("/feedback")]} />
-        <Button Text="Sair" onClick={() => [signout(), navigate("/")]} />
+        <C.ContentMenu>
+          Ubs Vac
+        </C.ContentMenu>
+        <C.ContentMenu>
+          <Button Text="Home" onClick={() => [getUbs(), navigate("/home")]} />
+        </C.ContentMenu>
+        <C.ContentMenu>
+          <Button Text="Encontrar UBS" onClick={() => [getUbs(), navigate("/ubs")]} />
+        </C.ContentMenu>
+        <C.ContentMenu>
+          <Button Text="Vacinas" onClick={() => [getUbsWithVacinas(), navigate("/vacinas")]} />
+        </C.ContentMenu>
+        <C.ContentMenu>
+          <Button Text="Feedback" onClick={() => [signout(), navigate("/feedback")]} />
+        </C.ContentMenu>
+        <C.ContentMenu>
+          <Button Text="Sair" onClick={() => [signout(), navigate("/")]} />
+        </C.ContentMenu>
       </C.Menu>
-      <C.MainContent>
-      <C.TitleUbs>Lista de UBS em Caxias do Sul - RS</C.TitleUbs>
-      <C.BoxFilter>
-        <Input
-          type="nome"
-          placeholder="Digite sua UBS"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />
-        {nome && (
-          <C.CleaningBotton onClick={clearInput}>x</C.CleaningBotton>
-        )}
-      </C.BoxFilter>
-      <C.UbsList>
-        {list.map((ubs) => (
-          <C.UbsItem key={ubs.id}>
-            <p><b>UBS {ubs.nome}</b></p>
-            <p><b>Endereço:</b> {ubs.endereco}</p>
-            <p><b>Horário:</b> De segunda à sexta das {ubs.horario_atendimento}.</p>
-            <p><b>Telefone:</b> {ubs.telefone}</p>
-          </C.UbsItem>
-        ))}
-      </C.UbsList>
-    </C.MainContent>
+
+      <C.MainContainer>
+        <C.MainContent>
+          <C.TitleUbs>Confira as UBS de Caxias do Sul - RS</C.TitleUbs>
+          <C.Div>Pesquise a UBS que deseja ver as informações ou navegue para encontrar:</C.Div>
+          <C.BoxFilter>
+            <Input
+              type="nome"
+              placeholder="Ex.: Centenário"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+            {nome && (
+              <C.CleaningBotton onClick={clearInput}>x</C.CleaningBotton>
+            )}
+          </C.BoxFilter>
+          <C.UbsList>
+            {list.map((ubs) => (
+              <C.UbsItem key={ubs.id}>
+                <p><b>UBS {ubs.nome}</b></p>
+                <p><b>Endereço:</b> {ubs.endereco}</p>
+                <p><b>Horário:</b> De segunda à sexta das {ubs.horario_atendimento}.</p>
+                <p><b>Telefone:</b> {ubs.telefone}</p>
+              </C.UbsItem>
+            ))}
+          </C.UbsList>
+        </C.MainContent>
+      </C.MainContainer>
     </C.Container>
   );
 };
